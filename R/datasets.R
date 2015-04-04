9ecdbe05155921280a007a0096e7a3072f7d8c8c
Vassilis Kostakos <vassilis.spam@kostakos.org>
2015-04-04 09:08:51 +0300

    First commit

---
+++ R/datasets.R
@@ -0,0 +1,16 @@
+#' SUS dataset
+#'
+#' a dataset for testing use of Roxygen2 to document datasets
+#'
+#' @name SUSresponses
+#' @docType data
+#'
+#' @usage data(SUSresponses)
+#'
+#' @format Contains 45 survey responses (in rows) to the 10-point SUS questionnaire.
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(SUSresponses)
+NULL
